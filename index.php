a6696b4e7f8e72ff0141972c954b67b732fe3a95
Vazcore <vazcore@live.ru>
2014-02-11 00:11:48 +0200

    Version 1.00

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
-<?php
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-require_once($yii);
-Yii::createWebApplication($config)->run();
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/clear.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Exo+2:400,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+		<title>Page_title</title>
+	</head>
+	<body>
+		<div id="page_cover">
+			<header>
+				<nav>
+					<ul>	
+						<li><a href="index.php">Главная</a></li>						
+					</ul>
+				</nav>
+				<div class="clr"></div>
+				<h3 id="site-name">Блог "Технологии SEO"</h3>
+				<img src="image/seo_logo.jpg" width="100" id="header-logo">
+			</header>
+			<section id="posts-section">
+				<article class="post-cover">
+					<div class="post-title"><a href="#">Tittle of the post</a></div>
+					<div class="post-date">10 февраля 2014, Alexey Gabrusev</div>
+					<div class="post-image"><img src="image/post/no_image.jpg" width="100%"></div>
+					<div class="post-preview-text">
+						<p>To create your first image blog post, click here and select 'Add & Edit Posts' > All Posts > This is the title of your first image post. Great looking images make your blog posts more visually compelling for your audience, and encourage readers to keep coming back.
+</p>
+					</div>
+					<div class="clr"></div>
+					<div class="post-button"><a href="#" class="b-read-next">Читать дальше</a></div>
+					<div class="clr"></div>
+				</article>
+			</section>
+			<footer></footer>
+		</div>
+	</body>
+</html>
